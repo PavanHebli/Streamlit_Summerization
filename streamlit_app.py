2dcff7946743c33c0aa220d190dcdef2125fb874
PavanHebli <pavanhebli@gmail.com>
2024-09-01 18:21:19 -0500

    added openAI support

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import streamlit as st
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.docstore.document import Document
 from langchain.chains.summarize import load_summarize_chain
+from langchain.llms.openai import OpenAI
 
 def check_env_var(variable_name):
     if variable_name in os.environ:
@@ -12,26 +13,46 @@ def check_env_var(variable_name):
     else:
         return False
     
-llm = ChatGroq(model="llama3-8b-8192")
+
+def LLMCallFunc(llm):
+    # Split the source text
+    text_splitter = CharacterTextSplitter()
+    texts = text_splitter.split_text(source_text)
+    docs = [Document(page_content=t) for t in texts[:3]]
+    chain = load_summarize_chain(llm, chain_type="map_reduce")
+    summary = chain.invoke(docs)
+    return summary["output_text"]
+
 st.title('Text Summerizer using LangChain & Groq')
 # del os.environ['GROQ_API_KEY']
 # if not check_env_var('GROQ_API_KEY'):
 #     os.environ["GROQ_API_KEY"] = getpass.getpass()
 #     llm_api = st.text_input("Groq or other LLM API Key", type="password")
+option = st.selectbox(
+    "Select the API provider ",
+    ("Selection", "Groq", "OpenAI")
+)
 llm_api = st.text_input("Groq or other LLM API Key", type="password")
+
 source_text = st.text_area("Source Text", height=200)
 # Check if the 'Summarize' button is clicked
 if st.button("Summarize"):
-    # else:
-    try:
-        # Split the source text
-        text_splitter = CharacterTextSplitter()
-        texts = text_splitter.split_text(source_text)
-        docs = [Document(page_content=t) for t in texts[:3]]
-        chain = load_summarize_chain(llm, chain_type="map_reduce")
-        summary = chain.invoke(docs)
+    if not llm_api.strip() or not source_text.strip():
+        st.write(f"Please complete the missing fields.")
+    else:
+        try:
+            
+            if option == "Groq":
+                llm = ChatGroq(model="llama3-8b-8192", api_key=llm_api)
+                summary=LLMCallFunc(llm)
+            elif option == "OpenAI":
+                llm = OpenAI(temperature=0, openai_api_key=llm_api)
+                summary=LLMCallFunc(llm)
+            else:
+                summary="Please select the API provider"
+            # Display summary
+            st.write(summary)
+        except Exception as e:
 
-        # Display summary
-        st.write(summary["output_text"])
-    except Exception as e:
-        st.write(f"Oops, Something just broke :( {e}")
+            st.write(f"Oops, Something just broke :(")
+            st.write(f"{e}")
